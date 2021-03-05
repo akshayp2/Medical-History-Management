4be7663ef2e4fe3154e2d2c8852edec1ab180b6b
sandeep <sandeep.vishwakarma.sfdc@gmail.com>
2021-03-05 09:41:27 +0530

    init commit

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const PORT = 9000;
+app.get('/',(req,res)=>res.send('Hello world'));
+
+app.listen(PORT,()=>console.log(`server is running at ${PORT}`));
+
+
