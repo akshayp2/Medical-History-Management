25425a411a7bc15702b2f96f6de9f198096446d2
AkshayPatil <akshaypatil19971710@gmai.com>
2021-03-27 22:37:40 +0530

    changes in front view

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -11,13 +11,13 @@ return(
     <h3 className="row justify-content-center subheader p-2">Our client</h3>
     <div className="row justify-content-center">
       <div className="col-xs-6 col-sm-6 col-md-3">
-        <OurClient imagesrc="http://wallpaperheart.com/wp-content/uploads/2018/08/4k-ultra-hd-wallpaper-6.jpg" title="abc" caption="With supporting text below as a natural lead-in to additional content."></OurClient>
+        <OurClient imagesrc="https://media.istockphoto.com/vectors/medical-team-wearing-face-masks-vector-id1214206519" title="Clinic" caption="We would like to work with you ."></OurClient>
       </div>
       <div className="col-xs-6 col-sm-6 col-md-3">
-        <OurClient imagesrc="http://wallpaperheart.com/wp-content/uploads/2018/08/4k-ultra-hd-wallpaper-6.jpg" title="abc" caption="With supporting text below as a natural lead-in to additional content."></OurClient>
+        <OurClient imagesrc="https://i.kym-cdn.com/entries/icons/original/000/033/734/cover12.jpg" title="Doctors" caption="lets work together ."></OurClient>
       </div>
       <div className="col-xs-6 col-sm-6 col-md-3">
-        <OurClient imagesrc="http://wallpaperheart.com/wp-content/uploads/2018/08/4k-ultra-hd-wallpaper-6.jpg" title="abc" caption="With supporting text below as a natural lead-in to additional content."></OurClient>
+        <OurClient imagesrc="https://graphicriver.img.customer.envatousercontent.com/files/239125793/preview.jpg?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=b6df449d85f629db918a15fe94a31c02" title="Guest" caption="hey join us ."></OurClient>
       </div>
     </div>
     </div>
