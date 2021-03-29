458ff556f0b678c121444089449d7342f054a81a
unati123 <40731693+unati123@users.noreply.github.com>
2021-03-30 01:31:58 +0530

    OTP Done (#1)
    Co-authored-by: Sandeep-vishwakarma-sfdc <68598524+Sandeep-vishwakarma-sfdc@users.noreply.github.com>

---
+++ server/controllers/sendotp.js
@@ -0,0 +1,22 @@
+const fast2sms = require('fast-two-sms');
+const express = require('express');
+var app=express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+require('dotenv').config();
+
+
+exports.otpverify= (req,res)=>{
+let num="1234567890";
+let OTP="";
+for(let i=0;i<4;i++)
+{
+    OTP+=num[Math.floor(Math.random()*10)];
+}
+
+const phone=req.body.phone;
+var options = {authorization : process.env.API_KEY , message : OTP ,  numbers :[phone]} ;
+const response= fast2sms.sendMessage(options);
+res.send('OTP sent');
+
+};
--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const PORT = 9000;
-app.get('/',(req,res)=>res.send('Hello world'));
-
+const otpRoutes=require('../server/routes/otp');
+//app.use("/",otpRoutes);
+app.use('/',otpRoutes);
+app.use(express.json());
 app.listen(PORT,()=>console.log(`server is running at ${PORT}`));
 
 
